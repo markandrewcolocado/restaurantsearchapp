9fb889511a1d306523750ad3087d1e782f76345c
DESKTOP-BD1HNTD\Mark <markandrew.colocado@gmail.com>
2023-07-09 04:44:10 +0800

    Adding navigation in App.js and boilerplate screen SearchScreen.js.

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+const { createAppContainer } = require("react-navigation");
+const { createStackNavigator } = require("react-navigation-stack");
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import SearchScreen from "./src/screens/SearchScreen";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen,
   },
-});
+  {
+    initialRouteName: "Search",
+    defaultNavigationOptions: {
+      title: "Business Search",
+    },
+  }
+);
+
+export default createAppContainer(navigator);
---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>Seach Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
