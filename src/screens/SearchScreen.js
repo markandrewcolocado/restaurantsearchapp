025fb63cec93a98b75d3bb90cdeb68360a9bd667
DESKTOP-BD1HNTD\Mark <markandrew.colocado@gmail.com>
2023-07-14 04:04:29 +0800

    Using EMPTY ELEMENT as fix to over expanding views.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -16,7 +16,7 @@ const SearchScreen = () => {
   };
 
   return (
-    <View style={{ flex: 1 }}>
+    <>
       <SearchBar
         term={term}
         // onTermChange={(newTerm) => setTerm(newTerm)}
@@ -41,7 +41,7 @@ const SearchScreen = () => {
           title="Big Spender"
         />
       </ScrollView>
-    </View>
+    </>
   );
 };
 
