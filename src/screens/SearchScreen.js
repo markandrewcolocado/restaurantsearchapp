0cdae215dba71e43a53158ee63bef98be0f6fa9c
DESKTOP-BD1HNTD\Mark <markandrew.colocado@gmail.com>
2023-07-13 21:59:16 +0800

    Making a request to the yelp API.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,34 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  const searchApi = async () => {
+    const response = await yelp.get("/search", {
+      params: {
+        limit: 50,
+        term,
+        location: "san jose",
+      },
+    });
+    setResults(response.data.businesses);
+  };
 
   return (
     <View>
       <SearchBar
         term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        onTermSubmit={() => console.log("term submitted")}
+        // onTermChange={(newTerm) => setTerm(newTerm)}
+        // onTermSubmit={() => searchApi()}
+        onTermChange={setTerm}
+        onTermSubmit={searchApi}
       />
       <Text>Seach Screen</Text>
-      <Text>{term}</Text>
+      <Text>{results.length}</Text>
     </View>
   );
 };
