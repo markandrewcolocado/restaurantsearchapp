f11a32c7b7ae5cb97b5dafb95f62cb9d470f4efd
DESKTOP-BD1HNTD\Mark <markandrew.colocado@gmail.com>
2023-07-14 02:19:52 +0800

    Extracting business logic to create a hook.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,41 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
-import yelp from "../api/yelp";
+import useResults from "../hooks/useResults";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const searchApi = async () => {
-    console.log("Hi there");
-    try {
-      const response = await yelp.get("/search", {
-        params: {
-          limit: 50,
-          term,
-          location: "san jose",
-        },
-      });
-      setResults(response.data.businesses);
-    } catch (error) {
-      setErrorMessage("Something went wrong.");
-    }
-  };
-
-  useEffect(() => {
-    searchApi("pasta");
-  }, []);
+  const [searchApi, results, errorMessage] = useResults();
 
   return (
     <View>
       <SearchBar
         term={term}
         // onTermChange={(newTerm) => setTerm(newTerm)}
-        // onTermSubmit={() => searchApi()}
         onTermChange={setTerm}
-        onTermSubmit={searchApi}
+        onTermSubmit={() => searchApi(term)}
+        // onTermSubmit={searchApi}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>{results.length}</Text>
